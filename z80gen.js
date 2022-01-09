f039d71de777de8b226a1639cd0d17e60ea5951e
Jos Kwanten <j.kwanten@tvilight.com>
2022-01-09 16:34:02 +0100

    Some more expirimenting

--- z80gen.js
+++ z80gen.js
@@ -40,83 +40,97 @@ function emitFetchDirect16(reg) {
     emitCode(`let val2 = this.r16[${reg}];`);
 }
 
+function emitStoreInNN8() {
+    emitCode(`this.memory.uwrite8(nn, val2);`);
+}
+
+function emitStoreInNN16() {
+    emitCode(`this.memory.uwrite16(nn, val2);`);
+}
+
+
+function emitStoreInReg8(reg) {
+    emitCode(`this.r8[${reg}] = val2;`);
+}
+
+function emitStoreInReg16(reg) {
+    emitCode(`this.r16[${reg}] = val2;`);
+}
 let registerNames = ['A', 'F', 'B', 'C', 'D', 'E', 'H', 'L', 'AF', 'BC', 'DE', 'HL', 'HL\'', 'IXh', 'IXl', 'IX+o', 'IY+o'];
 let aliases = ['n', 'nn', 'p', 'q'];
 
+
+function generateLD(r) {
+    //console.log(r);
+    let match = mnemonic.exec(r.Instruction);
+    if (!match) {
+        throw new Error('No match for ' + JSON.stringify(r));
+    } else {
+        emitComment(r.Instruction);
+
+        if (match.groups['operand2']) {
+            let operand2 = match.groups['operand2'];
+            let indirectMatch = indirect.exec(operand2)
+            if (indirectMatch) {
+                let reg = indirectMatch.groups['reg'];
+                if (reg === 'n') {
+                    emitFetchN();
+                } else if (reg === 'nn') {
+                    emitFetchNN();
+                }
+
+                if (reg.length == 1) {
+                    emitFetchIndirect8(reg);
+                } else {
+                    emitFetchIndirect16(reg);
+                }
+            } else {
+                if (operand2 === 'n') {
+                    emitFetchN();
+                } else if (operand2 === 'nn') {
+                    emitFetchNN();
+                } else if (operand2.length == 1) {
+                    emitFetchDirect8(operand2);
+                } else {
+                    emitFetchDirect16(operand2);
+                }
+            }
+        }
+
+        if (match.groups['operand']) {
+            let operand = match.groups['operand'];
+            let indirectMatch = indirect.exec(operand);
+            if (indirectMatch) {
+                let reg = indirectMatch.groups['reg'];
+                if (reg === 'nn') {
+                    emitStoreInNN16();
+                } else if (reg.length == 1) {
+                    emitStoreInReg8Indirect(reg);
+                } else {
+                    emitStoreInReg16Indirect(reg);
+                }
+            } else {
+                if (operand === 'n') {
+                    emitFetchN();
+                } else if (operand === 'nn') {
+                    emitFetchNN();
+                } else if (operand.length == 1) {
+                    emitStoreInReg8(operand);
+                } else {
+                    emitStoreInReg16(operand);
+                }
+            }
+        }
+
+        emitComment('END\n\n');
+    }
+}
 
 fs.createReadStream('Opcodes.csv')
     .pipe(csv({ separator: ';' }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
         results.filter(r => r.Instruction.indexOf('LD') == 0).forEach(r => {
-            //console.log(r);
-            let match = mnemonic.exec(r.Instruction);
-            if (!match) {
-                throw new Error('No match for ' + JSON.stringify(r));
-            } else {
-                emitComment(r.Instruction);
-
-                if (match.groups['operand2']) {
-                    let operand2 = match.groups['operand2'];
-                    let indirectMatch = indirect.exec(operand2)
-                    if (indirectMatch) {
-                        let reg = indirectMatch.groups['reg'];
-                        if (reg === 'n') {
-                            emitFetchN();
-                        } else if (reg === 'nn') {
-                            emitFetchNN();
-                        }
-
-                        if (reg.length == 1) {
-                            emitFetchIndirect8(reg);
-                        } else {
-                            emitFetchIndirect16(reg);
-                        }
-                    } else {
-                        if (operand2 === 'n') {
-                            emitFetchN();
-                        } else if (operand2 === 'nn') {
-                            emitFetchNN();
-                        } else if (operand2.length == 1) {
-                            emitFetchDirect8(operand2);
-                        } else {
-                            emitFetchDirect16(operand2);
-                        }
-                    }
-                }
-
-                if (match.groups['operand1']) {
-                    let operand1 = match.groups['operand1'];
-                    let indirectMatch = indirect.exec(operand1)
-                    if (indirectMatch) {
-                        let reg = indirectMatch.groups['reg'];
-                        if (reg === 'n') {
-                            emitFetchN();
-                        } else if (reg === 'nn') {
-                            emitFetchNN();
-                        }
-
-                        if (reg.length == 1) {
-                            emitFetchIndirect8(reg);
-                        } else {
-                            emitFetchIndirect16(reg);
-                        }
-                    } else {
-                        if (operand2 === 'n') {
-                            emitFetchN();
-                        } else if (operand2 === 'nn') {
-                            emitFetchNN();
-                        } else if (operand2.length == 1) {
-                            emitFetchDirect8(operand2);
-                        } else {
-                            emitFetchDirect16(operand2);
-                        }
-                    }
-                }
-
-                // this.add(this.r16[HL], this.r16[SP]);
-            }
-        });
-
-        emitComment('// End');
-    });
+            generateLD(r);
+        })
+    });    
