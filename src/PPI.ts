945ebff5af24efb0b5115c8ef1261653d37c3461
Jos Kwanten <j.kwanten@tvilight.com>
2022-01-22 23:15:39 +0100

    Not tested PPI

--- src/PPI.ts
+++ src/PPI.ts
@@ -5,16 +5,62 @@ export class PPI {
 // Slot selector.
 // Thi PPI handles keyboard handling and TBD.
     
+    keyboardMatrix: string[][];
+    keyboardState: boolean[][];
+    keyboardRow: number;
+
+    constructor() {
+        this.keyboardRow = 0;
+        this.keyboardState = new Array(11).fill(false).map(() => new Array(8).fill(false));
+        this.keyboardMatrix = [
+            ['7','6','5','4','3','2','1','0'],
+            [';','(','@','$','^','-','9','8'],
+            ['b','a','*','/','.',',',')',':'],
+            ['j','i','h','g','f','e','d','c'],
+            ['r','q','p','o','n','m','l','k'],
+            ['z','y','x','w','v','u','t','s'],
+            ['F3','F2','F1','?Kan','CapsLock','Alt','Control','Shift'],
+            ['Enter','Select','Backspace','?Stop','Tab','Escape','F5','F4'],
+            ['ArrowRight','ArrowDown','ArrowUp','ArrowLeft','Delete','?Insert','?Home','Space'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+        ]
+    }
+
     // Read keyboard matrix
     readA9() {
-        return 0;
+        let result = 0;
+        this.keyboardState[this.keyboardRow].forEach((k, i) => {
+            result |= k ? 1 << (7 - i) : 0;
+        });
+        return result;
     }
 
     writeAA(val: number) {
-        
+        this.keyboardRow = val % 11;
     }
 
     writeAB(val: number) {
 
+    }
+
+    onKeydown(keyName: string) {
+        this.keyboardMatrix.forEach((r, i) => {
+            r.forEach((k, j) => {
+                if (k === keyName) {
+                    this.keyboardState[i][j] = true;
+                }
+            })
+        });
+    }
+
+    onKeyup(keyName: string) {
+        this.keyboardMatrix.forEach((r, i) => {
+            r.forEach((k, j) => {
+                if (k === keyName) {
+                    this.keyboardState[i][j] = false;
+                }
+            })
+        });
     }
 }
